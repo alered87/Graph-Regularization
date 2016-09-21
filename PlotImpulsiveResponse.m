a8c79a0b980eb3a2c51bce4bea9078d869ce783d
Alessandro Rossi <rossi111@unisi.it>
2016-09-21 15:35:17 +0200

    Add files via upload

---
+++ PlotImpulsiveResponse.m
@@ -0,0 +1,21 @@
+function  G = PlotImpulsiveResponse(solutions,start_point,end_point)
+% PlotImpulsiveResponse : calculating and plot the Impulsive Response 
+%                         (Green's Function) of a linear differential
+%                         equations with constant coefficients
+%
+%     G = PlotImpulsiveResponse(solutions,start_point,end_point)
+%
+%     solutions: solutions of the characteristic poly of a linear 
+%                differential equations with constant coefficients
+%     start_point: starting point for the plot
+%     end_point: ending point for the plot
+%
+% Author: Alessandro Rossi (2016)
+%         rossi111@unisi.it
+
+syms s t ;
+
+F = 1/prod(s-solutions);
+G = ilaplace(F,s,t);
+
+ezplot(G,[start_point,end_point]);
